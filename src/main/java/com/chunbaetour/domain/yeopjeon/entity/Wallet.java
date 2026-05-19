5c14b5a8de470cbadfcba8af8e9795fc6403124d
hyeonmin02 <hyenmin5@gmail.com>
2026-05-19 17:42:03 +0900

    feat: BaseEntity 추가 및 Wallet 상속 적용
    - domain.common.entity.BaseEntity: createdAt/updatedAt 공통 추상 클래스
    - Wallet: BaseEntity 상속으로 auditing 필드 중복 제거

--- src/main/java/com/chunbaetour/domain/yeopjeon/entity/Wallet.java
+++ src/main/java/com/chunbaetour/domain/yeopjeon/entity/Wallet.java
@@ -1,26 +1,21 @@
 package com.chunbaetour.domain.yeopjeon.entity;
 
+import com.chunbaetour.domain.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "wallets")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
-public class Wallet {
+public class Wallet extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,14 +26,6 @@ public class Wallet {
 
     @Column(nullable = false)
     private Long balance;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
 
     public static Wallet create(Long userId) {
         Wallet wallet = new Wallet();
