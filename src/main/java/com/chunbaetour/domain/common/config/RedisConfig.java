774d9277cd48fb87e40c6310ba38c78fe8cf5cc1
hyeonmin02 <hyenmin5@gmail.com>
2026-05-19 19:40:59 +0900

    feat(KAN-45): Redisson 분산 락 설정 + 결제/스토어/상인/가게 ErrorCode 등록
    - build.gradle: redisson:3.35.0 의존성 추가
    - RedisConfig: RedissonClient 빈 등록 (분산 락용, destroyMethod=shutdown)
    - ErrorCode: PAY_001~011, STORE_001~007, MERCHANT_001~003, SHOP_001~006 추가

--- src/main/java/com/chunbaetour/domain/common/config/RedisConfig.java
+++ src/main/java/com/chunbaetour/domain/common/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package com.chunbaetour.domain.common.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,6 +15,20 @@ import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+              .setAddress("redis://" + redisHost + ":" + redisPort);
+        return Redisson.create(config);
+    }
 
     /**
      * 문자열 기반 Redis 연산용 템플릿.
