a237151ff5a66abd3d7f151c1a1fc7113280fdc3
임하은 <gkdmsdl100414@gmail.com>
2026-06-01 19:47:37 +0900

    fix: SupportRoomServiceTest 중복 assertThat import 제거 (KAN-196)

--- src/test/java/com/chunbaetour/domain/cs/service/SupportRoomServiceTest.java
+++ src/test/java/com/chunbaetour/domain/cs/service/SupportRoomServiceTest.java
@@ -1,7 +1,6 @@
 package com.chunbaetour.domain.cs.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
